2a0b676324fd828edc121bfd4b2a429055a0f644
Bar Bichachi <barbi2@mta.ac.il>
2025-08-30 16:06:56 +0300

    Added app.config.ts to github for vercel

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default {
+  name: "Swapit",
+  slug: "swapit",
+  version: "1.0.0",
+  extra: {
+    supabaseUrl: "https://fsnzptxquxzykcwvwhck.supabase.co",
+    supabaseKey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbnpwdHhxdXh6eWtjd3Z3aGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNTgwOTgsImV4cCI6MjA2OTYzNDA5OH0.7jj69WkjGib1RFFLqkwTEFNMZl_pz5gJm5WgK-Th8ME",
+  },
+};

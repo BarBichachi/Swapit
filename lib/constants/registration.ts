f3c9c035fb944ca2a15d154cbc2e1935bf3693d8
Yonty <yonathanpo@gmail.com>
2025-09-08 19:51:23 +0300

    little adjusts for registration constants

--- lib/constants/registration.ts
+++ lib/constants/registration.ts
@@ -1,9 +1,10 @@
 export const birthYears = Array.from(
-  { length: 2025 - 1920 + 1 },
-  (_, i) => 2025 - i
+  { length: 2007 - 1925 + 1 },
+  (_, i) => 2007 - i
 );
 
 export const genders = [
   { value: "Male", label: "Male" },
   { value: "Female", label: "Female" },
+  { value: "Rather not say", label: "Rather not say" },
 ];
